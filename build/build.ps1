cddf07ad09dea6d532cbc13e217bc5aa2a35d4d0
Morten <git@lerun.info>
2019-06-11 18:15:34 +0200

    Update build.ps1
    Update

--- build/build.ps1
+++ build/build.ps1
@@ -1,6 +1,6 @@
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 
-Register-PSRepository -Name "PSGallery" -SourceLocation "https://www.powershellgallery.com/api/v2/" -PublishLocation "https://www.powershellgallery.com/api/v2/package/" -InstallationPolicy 'Trusted'
+Register-PSRepository -Default -InstallationPolicy 'Trusted'
 
 Install-Module PSScriptAnalyzer -Scope CurrentUser
 Import-Module PSScriptAnalyzer
