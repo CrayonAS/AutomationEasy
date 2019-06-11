fea3d60d6a0d62337a12b9bb92af6d780d9df850
Morten <git@lerun.info>
2019-06-11 18:59:38 +0200

    Update build.ps1
    More problems

--- build/build.ps1
+++ build/build.ps1
@@ -1,9 +1,12 @@
 Write-Host "Running PS version: $($PSVersionTable.PSVersion)`nOn host: $($env:computername)"
-Install-PackageProvider -Name NuGet -Force #-MinimumVersion 2.8.5.201 -Verbose
 
-Register-PSRepository -Default -Verbose
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted -Verbose
+Get-Module -ListAvailable PackageManagement, PowerShellGet
+
+Unregister-PSRepository -Name 'PSGallery'
+Register-PSRepository -Default
 Get-PSRepository
 
 Install-Module PSScriptAnalyzer -Scope CurrentUser
 Import-Module PSScriptAnalyzer
+
+Install-PackageProvider -Name NuGet -Force #-MinimumVersion 2.8.5.201 -Verbose
