2256b3b3c97e8ae656792ef298ff34b3f4363df4
Morten <git@lerun.info>
2019-06-11 18:04:12 +0200

    Update build.ps1
    Added logging of available repo

--- build/build.ps1
+++ build/build.ps1
@@ -1,5 +1,9 @@
-﻿Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Install-Module -Name PowerShellGet -AllowClobber -Force -Repository PSGallery
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Write-Host "Available repos:`n" 
+Get-PSRepository
+
+Install-Module -Scope CurrentUser -Name PowerShellGet -AllowClobber -Force -Repository PSGallery
+Import-Module PowerShellGet
 Set-PSRepository PSGallery -InstallationPolicy Trusted
 
 Install-Module PSScriptAnalyzer -Scope CurrentUser
