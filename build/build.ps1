0eb6df05d1aa1a9ae156ae8c93cd1f3cdd75cdf0
Morten <git@lerun.info>
2019-06-11 18:11:17 +0200

    Update build.ps1
    Add PSGallery to trusted repo

--- build/build.ps1
+++ build/build.ps1
@@ -1,10 +1,6 @@
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Write-Host "Available repos:`n" 
-Get-PSRepository
 
-Install-Module -Scope CurrentUser -Name PowerShellGet -AllowClobber -Force -Repository PSGallery
-Import-Module PowerShellGet
-Set-PSRepository PSGallery -InstallationPolicy Trusted
+Register-PSRepository -Name "PSGallery" -SourceLocation "https://www.powershellgallery.com/api/v2/" -PublishLocation "https://www.powershellgallery.com/api/v2/package/" -InstallationPolicy 'Trusted'
 
 Install-Module PSScriptAnalyzer -Scope CurrentUser
 Import-Module PSScriptAnalyzer
