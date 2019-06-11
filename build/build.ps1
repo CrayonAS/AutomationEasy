140691747109b2f99a4effb2f7c5ae9c3f1c4c2d
Morten <git@lerun.info>
2019-06-11 19:09:25 +0200

    Update build.ps1
    PSGallery down --> build fail

--- build/build.ps1
+++ build/build.ps1
@@ -1,12 +1,12 @@
 Write-Host "Running PS version: $($PSVersionTable.PSVersion)`nOn host: $($env:computername)"
 
+Install-PackageProvider -Name NuGet -Force -MinimumVersion 2.8.5.201
+
 Get-Module -ListAvailable PackageManagement, PowerShellGet
 
-Unregister-PSRepository -Name 'PSGallery'
-Register-PSRepository -Default
+#Unregister-PSRepository -Name 'PSGallery'
+Register-PSRepository -Default -InstallationPolicy Trusted
 Get-PSRepository
 
 Install-Module PSScriptAnalyzer -Scope CurrentUser
 Import-Module PSScriptAnalyzer
-
-Install-PackageProvider -Name NuGet -Force #-MinimumVersion 2.8.5.201 -Verbose
