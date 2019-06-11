b8dc6784c41a1a320a0a92f514ea206d9e91528f
Morten <git@lerun.info>
2019-06-11 18:50:59 +0200

    Update build.ps1
    more testing to get PSGallery to work

--- build/build.ps1
+++ build/build.ps1
@@ -1,8 +1,9 @@
-Register-PSRepository -Default
-Get-PSRepository
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Write-Host "Running PS version: $($PSVersionTable.PSVersion)`nOn host: $($env:computername)"
+Install-PackageProvider -Name NuGet -Force #-MinimumVersion 2.8.5.201 -Verbose
 
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Register-PSRepository -Default -Verbose
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted -Verbose
+Get-PSRepository
 
 Install-Module PSScriptAnalyzer -Scope CurrentUser
 Import-Module PSScriptAnalyzer
