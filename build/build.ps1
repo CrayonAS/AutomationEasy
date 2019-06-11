b9ae84cf4783462d609165b959a902bf9e7411b5
Morten <git@lerun.info>
2019-06-11 17:57:32 +0200

    Update build.ps1
    Fix setting PSGallery as trusted repo

--- build/build.ps1
+++ build/build.ps1
@@ -1,4 +1,5 @@
 ﻿Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-Module -Name PowerShellGet -AllowClobber -Force -Repository PSGallery
 Set-PSRepository PSGallery -InstallationPolicy Trusted
 
 Install-Module PSScriptAnalyzer -Scope CurrentUser
