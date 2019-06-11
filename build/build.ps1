015e644878845afd03d9c11bf9056b6a73b37ec6
Morten <git@lerun.info>
2019-06-11 18:29:23 +0200

    Update build.ps1
    Fixes

--- build/build.ps1
+++ build/build.ps1
@@ -1,6 +1,8 @@
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Register-PSRepository -Default
+Get-PSRepository
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 
-Register-PSRepository -Default -InstallationPolicy 'Trusted'
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 
 Install-Module PSScriptAnalyzer -Scope CurrentUser
 Import-Module PSScriptAnalyzer
